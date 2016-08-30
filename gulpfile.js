8f4d49007f25fedb11239fdf152cbf1320fa58f8
Rahul Rai <rahulrai@live.com>
2016-08-30 18:18:34 +1000

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp')
 gulp.task('css', function() {
-  gulp.src('static/css/*.css')
-    .pipe(gulp.dest('static/css/'))
+  gulp.src('/static/css/*.css')
+    .pipe(gulp.dest('/static/css/'))
 })
 
 gulp.task('default', ['css'])
