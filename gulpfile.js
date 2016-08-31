ab8354afedd575297f60e7e5bef79be4a2ce4885
Rahul Rai <rahulrai@live.com>
2016-08-31 16:03:30 +1000

    minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp')
-gulp.task('css', function() {
-  gulp.src('/static/css/*.css')
-    .pipe(gulp.dest('/static/css/'))
-})
-
-gulp.task('default', ['css'])
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var es = require('event-stream');
+gulp.task('default', function() {
+	return es.merge(
+		gulp.src('expanded/css/*.css').pipe(cssmin()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('static/css/')),
+		gulp.src('themes/hugo-phlat-theme/static/css/highlightjs-themes/*.css').pipe(cssmin()).pipe(gulp.dest('static/css/highlightjs-themes/')),
+		gulp.src('themes/hugo-phlat-theme/static/css/phlat.css').pipe(cssmin()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('static/css/')));
+});
