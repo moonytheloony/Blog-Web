0ee0a7ff7ad7ed343f969d7ac75768d0549e8c4f
Rahul Rai <rahulrai@live.com>
2016-08-31 17:00:29 +1000

    Changed compression scheme

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
 var es = require('event-stream');
 var htmlmin = require('gulp-htmlmin');
-gulp.task('default', function() {
+
+gulp.task('minifyStylesheets', function() {
 	return es.merge(
-		gulp.src('expanded/css/*.css').pipe(cssmin()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('static/css/')),
+		gulp.src('static/css/*.css').pipe(cssmin()).pipe(gulp.dest('static/css/')),
 		gulp.src('themes/hugo-phlat-theme/static/css/highlightjs-themes/*.css').pipe(cssmin()).pipe(gulp.dest('static/css/highlightjs-themes/')),
-		gulp.src('themes/hugo-phlat-theme/static/css/phlat.css').pipe(cssmin()).pipe(rename({suffix: '.min'})).pipe(gulp.dest('static/css/')),
-		gulp.src('public/**/*.html').pipe(htmlmin({collapseWhitespace: true})).pipe(gulp.dest('public')));
+		gulp.src('themes/hugo-phlat-theme/static/css/phlat.css').pipe(cssmin()).pipe(gulp.dest('static/css/')));
+});
+
+gulp.task('minifyHtml', function() {
+	return gulp.src('public/**/*.html').pipe(htmlmin({collapseWhitespace: true})).pipe(gulp.dest('public'));
 });
