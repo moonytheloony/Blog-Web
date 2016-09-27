45122e59dcf6ad4809c57a6016506f037f928e0c
Rahul Rai <rahulrai@live.com>
2016-09-27 12:57:06 +1000

    Add custom tracking

---
+++ content/post/Moving to Hugo Sticking To Azure.md
@@ -0,0 +1,11 @@
+﻿+++
+author = "Rahul Rai"
+categories = ["azure", "storage"]
+date = "2016-05-04T17:04:47+10:00"
+draft = true
+tags = ["azure storage", "cloud service", "file", "github", "storage", "webapp", "website", "mount", "file share", "smb"]
+title = "Moving to Hugo Sticking to Azure"
+type = "post"
++++
+
+I recently completed 
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,30 @@
-function validateKey()
-{
-	var inputValue = document.getElementById('passkey').value;
-	if (inputValue.length == "5") 
-	{
-		var completeUrl = 'https://onedrive.live.com/embed?cid=DFF1B4D9CD55ADCA&resid=DFF1B4D9CD55ADCA%2126211&authkey=' + inputValue + 'vRhKe6BOaU&em=2'
-		var theFrame = document.getElementById("my_iframe");
-		theFrame.src = completeUrl;
-		theFrame.style.display = 'block';
-	}
-	else
-	{
-		theFrame.style.display = 'none';
-	}
-}
+function validateKey() {
+    var inputValue = document.getElementById('passkey').value;
+    if (inputValue.length === "5") {
+        var completeUrl =
+            'https://onedrive.live.com/embed?cid=DFF1B4D9CD55ADCA&resid=DFF1B4D9CD55ADCA%2126211&authkey=' +
+                inputValue +
+                'vRhKe6BOaU&em=2';
+        var theFrame = document.getElementById("my_iframe");
+        theFrame.src = completeUrl;
+        theFrame.style.display = 'block';
+    }
+    else {
+        theFrame.style.display = 'none';
+    }
+};
+
+function triggerWaypoint(logEventInHeap) {
+    var waypoint = new Waypoint({
+        element: document.getElementById("waypoint-element"),
+        handler: function () {
+            if (logEventInHeap) {
+                heap.track("Read To End", { post: window.location.pathname });
+            } else {
+                alert('Event Logged' + window.location.pathname);
+            }
+
+            this.destroy();
+        }
+    });
+};
