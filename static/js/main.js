dea007c05b623134479e970f207c40ccb4768791
Rahul Rai <rahulrai@live.com>
2016-10-06 16:54:16 +1100

    fixed blank search term issue

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,12 @@
 function validateKey() {
     var inputValue = document.getElementById('passkey').value;
+    var theFrame;
     if (inputValue.length === "5") {
         var completeUrl =
             'https://onedrive.live.com/embed?cid=DFF1B4D9CD55ADCA&resid=DFF1B4D9CD55ADCA%2126211&authkey=' +
                 inputValue +
                 'vRhKe6BOaU&em=2';
-        var theFrame = document.getElementById("my_iframe");
+        theFrame = document.getElementById("my_iframe");
         theFrame.src = completeUrl;
         theFrame.style.display = 'block';
     }
@@ -28,3 +29,17 @@ function triggerWaypoint(logEventInHeap) {
         }
     });
 };
+
+$(document).ready(function () {
+    $("#search")
+        .submit(function (event) {
+            var searchBox = $('[name="q"]');
+            if (searchBox.val().trim() === "") {
+                $("#search").addClass("has-error");
+                event.preventDefault();
+            } else {
+                $("#search").removeClass("error");
+                return;
+            }
+        });
+});
