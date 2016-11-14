ff4ac1801286d63fca0add6f5ed64615c7d6e7ba
Rahul Rai <rahulrai@live.com>
2016-11-14 16:33:52 +1100

    updated subscribe

--- static/js/main.js
+++ static/js/main.js
@@ -34,11 +34,13 @@ $(document).ready(function () {
     $("#subscribeNewsletter").click(function (e) {
         e.preventDefault();
         $("#subscribeNewsletterModal").modal("show");
+        createCookie("newsletterReminder", "true", 20);
         return;
     });
     $("#subscribeFooterLink").click(function (e) {
         e.preventDefault();
         $("#subscribeNewsletterModal").modal("show");
+        createCookie("newsletterReminder", "true", 20);
         return;
     });
     $("a").click(function (e) {
@@ -46,12 +48,14 @@ $(document).ready(function () {
         if (fragment === "subscribe") {
             e.preventDefault();
             $("#subscribeNewsletterModal").modal("show");
+            createCookie("newsletterReminder", "true", 20);
         }
         return;
     });
     var subscribeFragment = window.location.hash.substr(1);
     if (subscribeFragment === "subscribe") {
         $("#subscribeNewsletterModal").modal("show");
+        createCookie("newsletterReminder", "true", 20);
     }
 });
 
@@ -91,3 +95,43 @@ function drawChart(chartArray) {
         }
     }
 };
+
+function createCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEq = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEq) === 0) {
+            return c.substring(nameEq.length, c.length);
+        }
+    }
+
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+$(document).ready(function () {
+    setTimeout(cookieReminder, 60000);
+    function cookieReminder() {
+        var visited = readCookie("newsletterReminder");
+        if (!visited || visited !== "true") {
+            createCookie("newsletterReminder", "true", 20);
+            $("#subscribeNewsletterModal").modal("show");
+        }
+    }
+});
